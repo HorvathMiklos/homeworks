64a902c9c365bac66ccf902181cc6078ca512844
Horváth Mikls <horvathmikloslevelez4@gmail.com>
2017-02-02 20:21:02 +0100

    write test scripts

---
+++ EJBhw/scripts/04-login_2.sh
@@ -0,0 +1,1 @@
+curl --verbose --write-out "\n" --request POST --data @02-create_user.json --header "Content-Type: application/json" --cookie-jar cart-cookies.txt --cookie cart-cookies.txt http://localhost:8080/EJBhomework-web/web/users/login
